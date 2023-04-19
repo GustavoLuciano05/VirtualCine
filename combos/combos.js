fb8932ffd76440048939f5f61d86a51d59052888
Antonio <antonio_b_lima@estudante.sesisenai.org.br>
2023-04-19 07:52:03 -0300

    kayque é lindo

--- combos/combos.js
+++ combos/combos.js
@@ -9,16 +9,17 @@ function carrinho(){
   
     i++;
      
-     if(i > 0) {
+     if(i === 1) {
         carrinho.style.position = 'fixed';
         carrinho.style.width = '30%';
         carrinho.style.height = '50%';
-        i = 0;
+        
      } else {
         carrinho.style.position = 'relative';
         carrinho.style.width = '70%';
         carrinho.style.height = '90%';
         carrinho.classList.add("carrinho");
+        i = 0;
      }
   
 }
