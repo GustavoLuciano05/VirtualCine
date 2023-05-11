7346daca2ea47d55d6f3a120f4f2261b706a28d5
GustavoLuciano05 <gustavo_l_de-souza@estudante.sesisenai.org.br>
2023-05-11 12:28:05 -0300

    at

---
+++ recuperação_senha/recuperacao.js
@@ -0,0 +1,40 @@
+function MostrarCheckbox(id)
+{
+    if(id=="MensagemSMS")
+    {
+        telefone.style.display="block";
+        usuario.style.display="none";
+
+
+    }
+    if(id=="Emailcaixa")
+    {
+        telefone.style.display="none";
+        usuario.style.display="block";
+
+    }
+
+    if(id=="MensagemSMS")
+    {
+        caixabranca.style.display="none";
+        telefone.style.display="block";
+
+    }
+
+    if(id=="Emailcaixa")
+    {
+        caixabranca.style.display="none";
+        usuario.style.display="block";
+    } 
+}  
+    
+     
+      
+
+        
+    
+
+    
+
+
+
