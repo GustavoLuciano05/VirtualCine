6179c2bec5d8e5fcdd0e0af9e0cfe02dec036cfc
Antônio Budal <antonio_b_lima@estudante.sesisenai.org.br>
2023-05-11 21:05:52 -0300

    Fazendo o cabecalho ser scrolavel

--- telaInicial/novaTelaInicial.js
+++ telaInicial/novaTelaInicial.js
@@ -30,8 +30,9 @@ function VerificaBotoes() {
       document.getElementById('cadastro').style.display = 'none';
       document.getElementById('assinar').style.display = 'inline-block';
   }
-}
 
+  
+}
 function telafilme(i){
 
     if (variavel == undefined) {
@@ -49,17 +50,45 @@ function mostrarMenu(id,val){
     j++;
     if (j == 1)  {
         $('#'+id).css('z-index',2);
-        $('#'+id).animate({top: val},1000);
-        $('#'+id).siblings().animate({top: '0%'},500);
+        
+        $('#'+id).animate({opacity:1,top: (val)}, 1000);
+        // $('#'+id).animate({},1000);
+        $('#'+id).siblings().animate({top: '0%',opacity:0},500);
+        
     }
     else { 
-        $('#'+id).animate({top: '0%'},500);
+        
         j = 0;
+        $('#'+id).animate({top: '-100%',opacity:0},500);
         $('#'+id).css('z-index',1);
+       
+        
     } 
 }
 
+ 
+
+function FecharPerfil(){
+    $('#menuPerfil').animate({"z-index": "-2"},10);
+    $('#menuPerfil').animate({"display": "none"},200);
+    $('#menuPerfil').animate({"height": "0%"},200);
+   
+   
+  
+}
+function MostrarPerfil(){
+   
+    $('#menuPerfil').animate({height: '100%'},100);
+   
+    $('#menuPerfil').css("display", "flex");
+  
+    $('#menuPerfil').animate({"z-index": "20"},10);
+}
+
+
+
 var leftAtual = 0;
+
 
 function voltarSlide(){
     leftAtual+=1024;
@@ -96,6 +125,12 @@ function avancarSlide(){
     
 }
 
+setInterval(function() {
+    $('#slides').animate({left:'-80%'},5000);
+    $('#slides').animate({left:'-160%'},5000);
+    $('#slides').animate({left:0},1000);
+  
+  }, 3000);
 
 function abrirTela(pasta,arquivo){
     let str = '../'+pasta+'/'+arquivo;
