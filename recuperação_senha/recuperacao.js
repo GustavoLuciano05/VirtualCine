8500e06f4366990b0acd1de5f25bf94042d6438c
GustavoLuciano05 <gustavo_l_de-souza@estudante.sesisenai.org.br>
2023-05-22 12:19:03 -0300

    ATUALIZACOES-ULTIMA

--- recuperação_senha/recuperacao.js
+++ recuperação_senha/recuperacao.js
@@ -1,40 +1,61 @@
-function MostrarCheckbox(id)
-{
-    if(id=="MensagemSMS")
-    {
-        telefone.style.display="block";
-        usuario.style.display="none";
+function MostrarCheckbox(id) {
+    if (id == "MensagemSMS") {
+        telefone.style.display = "block";
+        usuario.style.display = "none";
 
 
     }
-    if(id=="Emailcaixa")
-    {
-        telefone.style.display="none";
-        usuario.style.display="block";
+    if (id == "Emailcaixa") {
+        telefone.style.display = "none";
+        usuario.style.display = "block";
 
     }
 
-    if(id=="MensagemSMS")
-    {
-        caixabranca.style.display="none";
-        telefone.style.display="block";
+    if (id == "MensagemSMS") {
+        caixabranca.style.display = "none";
+        telefone.style.display = "block";
 
     }
 
-    if(id=="Emailcaixa")
-    {
-        caixabranca.style.display="none";
-        usuario.style.display="block";
-    } 
-}  
-    
-     
-      
+    if (id == "Emailcaixa") {
+        caixabranca.style.display = "none";
+        usuario.style.display = "block";
+    }
+}
 
-        
-    
+function exibirBandeiras(){
+    var lista = document.getElementById("lista-bandeiras");
+    if (lista.style.display === "none") {
+      lista.style.display = "block";
+    } else {
+      lista.style.display = "none";
+    }
+  }    
+
+function trocaBandeiras(){
+    var lista = document.getElementById("troca-bandeiras");
+    if (lista.style.display === "none") {
+      lista.style.display = "block";
+    } else {
+      lista.style.display = "none";
+    }
+  }    
 
     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
