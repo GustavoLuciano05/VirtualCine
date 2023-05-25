55cbdc5bf6d93da1a4e67b57225f804c54e0bc69
GustavoLuciano05 <you@example.com>
2023-05-25 10:18:36 -0300

    dadafsfgs

--- recuperação_senha/recuperacao.js
+++ recuperação_senha/recuperacao.js
@@ -32,27 +32,46 @@ function exibirBandeiras(){
     }
   }    
 
-function trocaBandeiras(){
-    var lista = document.getElementById("troca-bandeiras");
-    if (lista.style.display === "none") {
-      lista.style.display = "block";
-    } else {
-      lista.style.display = "none";
-    }
-  }    
+//   function trocarBandeiras(){
+//     var lista = document.getElementById("trocaBandeira");
+//     var imagem = document.getElementById('2-argentina')
+//     if (lista.style.display === "none") {
+//         lista.style.display = "block";
+//       } else {
+//         lista.value = imagem
+//       }
+//   }
 
+//   function trocarBandeiras() {
+//     var lista = document.getElementById("trocaBandeira");
+//     var imagem = document.getElementById('2-argentina');
     
+//     if (lista.style.display === "none") {
+//       lista.style.display = "block";
+//     } else {
+//       lista.value = imagem.src("recuperação_senha/img/argentina.png");
+//     }
+//   }
+ 
+// const caixaBandeira = document.querySelector('.caixadabandeira');
+// const paises = document.querySelectorAll('.paises .pais');
 
+// paises.forEach(pais => {
+//   const imgPais = pais.querySelector('img');
 
+//   pais.addEventListener('click', function() {
+//     const imgSrc = imgPais.src; 
 
+//     caixaBandeira.querySelector('.bandeira').src = imgSrc;
+//   });
+// });
 
-
-
-
-
-
-
-
+function trocarBandeiras(src, id, n) {
+    var imagem = document.getElementById('trocaBandeira');
+    var imagemPais = document.getElementById(id);
+    imagem.src = src;
+    dddPais.value = n
+  }
 
 
 
